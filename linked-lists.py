953677a6cb2aaf3b76c7674f005ab85a0e8669d2
Kevin Dsouza <kevin.dsouza@cix.csi.cuny.edu>
2024-07-08 14:30:09 -0400

    added reverse a linked list and a printer helper function

--- linked-lists.py
+++ linked-lists.py
@@ -54,32 +54,68 @@ def search(head,val):
         print(f"Error has occured: {e}")
         return head
 
+def reverse_linked_list(head):
+    prev = None
+    current = head
+    try:
+        while current is not None:
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
+        head = prev
+        return head
+    except Exception as e:
+        print(f"An error has occured {e}")
+        return None
+
+def print_linked_list(head):
+    if head is None:
+        print("Empty list")
+        return
+    s = ""
+    while head is not None:
+        s += str(head.val) + " -> "
+        head = head.next
+    s += "None"
+    print(s)
+
 def main():
-    arr = list(range(100000000))
+    # arr = list(range(100000000))
 
-    #Remove num time from array
-    print("Array Removal")
-    start_time = datetime.now()
-    arr.remove(853)
-    end_time = datetime.now()
-    print(f"Execution time:{end_time - start_time}")
+    # #Remove num time from array
+    # print("Array Removal")
+    # start_time = datetime.now()
+    # arr.remove(853)
+    # end_time = datetime.now()
+    # print(f"Execution time:{end_time - start_time}")
 
 
-    #Populate our Linked List the same way
+    # #Populate our Linked List the same way
 
+    # head = Node(0,None)
+    # iter = head
+    # for i in range(1,100000000):
+    #     iter.next = Node(i,None)
+    #     iter = iter.next
+
+    # #Now remove same 853 from LinkedList
+
+    # print("Linked List Removal")
+    # start_time = datetime.now()
+    # remove_node(head,853)
+    # end_time = datetime.now()
+    # print(f"Execution time:{end_time - start_time}")
+
+    
     head = Node(0,None)
     iter = head
-    for i in range(1,100000000):
+    for i in range(1,10):
         iter.next = Node(i,None)
         iter = iter.next
-
-    #Now remove same 853 from LinkedList
-
-    print("Linked List Removal")
-    start_time = datetime.now()
-    remove_node(head,853)
-    end_time = datetime.now()
-    print(f"Execution time:{end_time - start_time}")
+    print_linked_list(head)
+    reversed_head = reverse_linked_list(head)
+    print_linked_list(reversed_head)
 
 if __name__ == "__main__":
     main()
